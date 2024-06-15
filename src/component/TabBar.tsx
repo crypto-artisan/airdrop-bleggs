40ef1a5ef82dcca8838d86762e5a37ab63502418
hightecular95908 <rkulkin47@gmail.com>
2024-06-16 05:44:58 +0900

    tabBarPadding

--- src/component/TabBar.tsx
+++ src/component/TabBar.tsx
@@ -7,7 +7,7 @@ interface TabProps {
 
 const TabBar: React.FC<TabProps> = ({ tab, setTab }) => {
     return (
-        <div className="flex flex-row justify-between absolute md:w-[40%] w-full h-[60px] bottom-0 bg-black-100 items-center border-t-[1px] border-gray">
+        <div className="flex flex-row justify-between absolute md:w-[40%] w-full h-[60px] bottom-0 bg-black-100 items-center border-t-[1px] border-gray px-4">
             <img src="/image/home.png" onClick={() => setTab("Home")} className={`${tab == 'Home' && 'scale-125 border-white border-b-2'} cursor-pointer w-10 h-10 hover:scale-125 transition-all duration-300`} />
             <img src="/image/task.png" onClick={() => setTab("Task")} className={`${tab == 'Task' && 'scale-125 border-white border-b-2'} cursor-pointer w-10 h-10 hover:scale-125 transition-all duration-300`} />
             <img src="/image/ranking.png" onClick={() => setTab("Ranking")} className={`${tab == 'Ranking' && 'scale-125 border-white border-b-2'} cursor-pointer w-10 h-10 hover:scale-125 transition-all duration-300`} />
